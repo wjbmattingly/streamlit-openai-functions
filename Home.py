77d8337c2be2d0eae6697aaf35c0347c7ae87fba
William Mattingly <62964060+wjbmattingly@users.noreply.github.com>
2023-06-23 18:44:07 +0200

    fixed typo
    fixed typo in Example

--- Home.py
+++ Home.py
@@ -5,7 +5,7 @@ import json
 open_ai_key = st.text_input("OpenAI API Key")
 openai.api_key = open_ai_key
 st.title("OpenAI Function Prototype Creation App")
-display_template = st.expander("Show OpenAI Examplel")
+display_template = st.expander("Show OpenAI Example")
 display_template.markdown(
 
     """
